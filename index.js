c424e99353ab6c713b27231d3876cc0c802d626b
vann19 <ahmadfansya660@gmail.com>
2024-10-04 11:14:55 +0700

    Mengenal Object Request Dan Response

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(() => {
-    console.log('Hello express')
-})
+app.use((req, res) => {
+    console.log('Berhasil')
+    res.send('string biasa')
+});
 
 app.listen(8080, () => {
     console.log('server di running di port: http://localhost:8080')
-})
+});
