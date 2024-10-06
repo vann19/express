7cd9c1ad4f7c6f3849d2dad6e16077ee6f8eeaf7
vann19 <ahmadfansya660@gmail.com>
2024-10-06 15:37:11 +0700

    Membuat Route Untuk Menentukan Response Tertentu

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-    console.log('Berhasil')
-    res.send('string biasa')
+// app.use((req, res) => {
+//     console.log('Berhasil')
+//     res.send('string biasa')
+// });
+
+app.get('/', (req, res) => {
+    res.send('ini adalah halaman home')
+});
+
+app.get('/about', (req, res) => {
+    res.send('ini adalah halaman about')
+});
+
+app.post('/about', (req, res) => {
+    res.send('ini adalah post about')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('ini adalah halaman Contact')
+});
+
+app.get('/*', (req, res) => {
+    res.send('Halaman tidak ditemukan')
 });
 
 app.listen(8080, () => {
